62fdc78ae302e90f2ece76b8ce7f590d8d74918e
KingHoeyoung <hoe925@g.skku.edu>
2021-07-04 20:01:25 +0900

    ~ App - 파일생성과 리다이렉션
    여기까지 들은 코드

---
+++ web2-nodejs/syntax/array.js
@@ -0,0 +1,10 @@
+//CRUD
+
+var arr = ['A', 'B', 'C', 'D']; //create
+console.log(arr[1]);
+console.log(arr[3]); //read
+arr[2] = 3;
+console.log(arr); //update
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
