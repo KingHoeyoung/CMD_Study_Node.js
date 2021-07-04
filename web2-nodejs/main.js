73e5d82ca23507c8e394a93ca581bcf48ae2cbfe
KingHoeyoung <hoe925@g.skku.edu>
2021-07-04 21:54:43 +0900

    ~ App - 입력 정보에 대한 보안
    여기까지 들은 코드.
    차후에 오류 있음을 발견.

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -28,7 +28,8 @@ var app = http.createServer(function(request,response){
       } else {
 
         fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.get('id')}`, 'utf8', function(err, description){
+          var filteredId = path.parse(queryData.get('id')).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.get('id');
             var list = template.list(filelist);
             var html = template.HTML(title, list, 
@@ -86,7 +87,8 @@ var app = http.createServer(function(request,response){
     } else if (pathname === '/update') {
 
       fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.get('id')}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.get('id')).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.get('id');
           var list = template.list(filelist);
           var template = template.HTML(title, list, `
@@ -137,7 +139,8 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`, function(error){
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error){
           response.writeHead(302, {Location: `/`});
           response.end();
         });
