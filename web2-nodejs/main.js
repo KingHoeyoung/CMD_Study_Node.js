7df1fdf9c3dae696692b242506dae02c9a6b8444
KingHoeyoung <hoe925@g.skku.edu>
2021-07-04 20:11:25 +0900

    ~ App - 글수정 - 수정 링크 생성

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -37,8 +37,6 @@ var app = http.createServer(function(request,response){
     var queryData = new URL('http://localhost:3000' + _url).searchParams;
     var pathname = new URL('http://localhost:3000' + _url).pathname;
 
-    //console.log(new URL('http://localhost:3000' + _url));
-
     if (pathname === '/'){
 
       if (queryData.get('id') === null){
@@ -47,7 +45,10 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          var template = templateHTML(title, list, 
+            `<h2>${title}</h2><p>${description}</p>`,
+            `<a href="/create">create</a>`
+          );
           response.writeHead(200);
           response.end(template);
         });
@@ -58,7 +59,10 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.get('id')}`, 'utf8', function(err, description){
             var title = queryData.get('id');
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+            var template = templateHTML(title, list, 
+              `<h2>${title}</h2><p>${description}</p>`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
             response.writeHead(200);
             response.end(template);
           });
@@ -81,7 +85,7 @@ var app = http.createServer(function(request,response){
             <input type="submit">
           </p>
         </form>
-        `);
+        `, '');
         response.writeHead(200);
         response.end(template);
       });
